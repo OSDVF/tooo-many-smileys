538571a423e2a72ad67c02c07c357c0b1121f36c
OSDVF <o.s.dv.f@seznam.cz>
2021-01-25 19:05:15 +0100

    Drobná úprava stránkovacího slovníku

--- .vuepress/theme/locales/cs.js
+++ .vuepress/theme/locales/cs.js
@@ -8,8 +8,8 @@ export default {
   pagation: {
     prev: 'Předchozí',
     next: 'Následující',
-    go: 'Otevřít',
-    jump: 'Ukázat'
+    go: 'Listovat',
+    jump: 'Stránka'
   },
   tag: {
     all: 'Všechny'
